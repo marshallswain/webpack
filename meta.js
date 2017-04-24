483fe63d6f02cf10ee1202af7677ec8095e94501
Marshall Thompson <marshall@creativeideal.net>
2017-04-24 12:03:59 -0600

    Always install testing frameworks

--- meta.js
+++ meta.js
@@ -69,23 +69,11 @@ module.exports = {
           "short": "none"
         }
       ]
-    },
-    "unit": {
-      "type": "confirm",
-      "message": "Setup unit tests with Testee + Mocha?"
-    },
-    "e2e": {
-      "type": "confirm",
-      "message": "Setup e2e tests with FuncUnit?"
     }
   },
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
     "src/router/**/*": "router"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
